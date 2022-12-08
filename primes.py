d58c742075d22f7ddff0ae89aafcf16d125b64d4
Ryan Tsoi <sanwingtsoi@gmail.com>
2022-12-08 06:06:00 +0000

    finish

--- primes.py
+++ primes.py
@@ -1,6 +1,22 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
+import math
 
 def primes(number_of_primes):
     list = []
+    if number_of_primes <= 0:
+        raise ValueError('')
+    else:
+        count = 0
+        num = 2
+        isPrime = True
+        while count<number_of_primes:
+            for i in range (2,  num ):
+                    if num % i==0:
+                        isPrime = False
+            if isPrime == True:
+                list.append(num)
+                count +=1
+            num+=1
+            isPrime = True
     return list
